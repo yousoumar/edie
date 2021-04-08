40a7b9155013a3a8b35c7027c2c2ca516bd19f69
Youssouf Oumar <63708012+yousoumar@users.noreply.github.com>
2021-04-09 00:14:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+C'est la page d'accueil d'une agence digitale, **Edie**, `mobile first`, `responsive`, développée en `HTML`, `CSS`, et `JavaScript`.
+
+C'est un <a href="https://devchallenges.io/challenges/xobQBuf8zWWmiYMIAZe0">`challenge`</a> de `devchallenge.io`.
+
+Visiter le site en cliquant sur l'image ci-dessous ou sur le lien en bas de la section `About`.
+
+`Note : il s'agit là d'une ancienne réalisation dont je viens de nettoyer l'historique des commits.`
+
+<a href = "https://yousoumar.github.io/edie/"><img src = "images/screenshot.png"></img></a>
---
+++ js/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const header = document.querySelector('header');
+const hamburger = document.querySelector('#hamburger');
+hamburger.addEventListener('click', ()=>{
+    header.classList.toggle('clicked');
+});
+const headerLinks = document.querySelectorAll('header a');
+headerLinks.forEach((headerLink) =>{
+    headerLink.addEventListener('click', ()=>{
+        header.classList.remove('clicked');
+    });
+});
+window.onscroll = ()=>{
+    if (window.pageYOffset>30){
+    header.style.boxShadow =" 0 0 .2rem black";
+    }else{
+    header.style.boxShadow ="none";
+    }
+};
